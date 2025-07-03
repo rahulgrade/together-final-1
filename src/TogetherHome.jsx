02dfe4f7c04bd7bbafa8fb0ed680439501f7c780
Rahul Kumar <103729786+rahulgrade@users.noreply.github.com>
2025-07-03 16:26:36 +0530

    Add files via upload

--- src/TogetherHome.jsx
+++ src/TogetherHome.jsx
@@ -53,7 +53,7 @@ export default function TogetherHome() {
         </button>
 
         {/* Desktop Menu */ }
-        <div className=" ml-auto hidden md:flex space-x-4">
+        <div className="ml-auto hidden md:flex space-x-4">
           <NavButtons />
         </div>
         <button className="ml-4 px-4 py-2 rounded font-semibold border border-black-600 text-black-600 hover:bg-black-50">
